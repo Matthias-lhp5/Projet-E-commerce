5da23f09ce048f76d12ccd6678d7aed1543babc4
Matthias-lhp5 <72194766+Matthias-lhp5@users.noreply.github.com>
2020-11-23 09:51:47 +0100

    recup script

---
+++ Assets/script.js
@@ -0,0 +1,51 @@
+var lienForTel = document.getElementById('lienForTel');
+var lienForAcceuil = document.getElementById('lienForAcceuil');
+
+var contentForTel = document.getElementById('contentForTel');
+var contentForAcceuil = document.getElementById('contentForAcceuil');
+
+
+lienForTel.addEventListener('click', function(){
+
+    contentForAcceuil.classList.remove('visibility');
+    contentForTel.classList.add('visibility');
+
+});
+
+lienForAcceuil.addEventListener('click', function(){
+
+    contentForTel.classList.remove('visibility');
+    contentForAcceuil.classList.add('visibility');
+
+});
+
+
+
+let choixCouleur = document.getElementsByClassName('choixCouleur');
+let choixCouleurArray = [... choixCouleur]
+
+
+choixCouleurArray.forEach(element => {
+
+    element.addEventListener('click', function(){
+        console.log(this)
+    })
+});
+
+
+test = document.getElementById('test');
+selector = document.getElementById('selector');
+
+test.addEventListener('blur', function(){
+    console.log(test)
+})
+
+
+var admettonsRecupImg = "assets/img/apple-iphone-11-pro-max.png";
+var admettonsRecupName = 'Iphone 11 Pro Max';
+var admettonsRecupPrix = 1150;
+
+var recupLignePanier = document.getElementById('recupLignePanier');
+
+console.log(recupLignePanier);
+
